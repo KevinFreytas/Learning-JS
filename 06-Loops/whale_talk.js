34690d14c3333986f5eed20287d15ac000331cfc
Kevin Freitas <kevinfreitas14@gmail.com>
2021-09-04 15:27:58 -0300

    new folder and files
    Take a phrase like ‘turpentine and turtles’ and translate it into its “whale talk” equivalent: ‘UUEEIEEAUUEE’.
    
    There are a few simple rules for translating text to whale language:
    
    There are no consonants. Only vowels excluding “y”.
    The u‘s and e‘s are extra long, so we must double them in our program.
    Once we have converted text to the whale language, the result is sung slowly, as is a custom in the ocean.
    
    To accomplish this translation, we can use our knowledge of loops. Let’s get started!

---
+++ 06-Loops/whale_talk.js
@@ -0,0 +1,26 @@
+// This variable will contain the text you want to translate into "whale talk".
+let input = 'a whale of a deal!'.toUpperCase();
+
+// Array of vowels to be extracted from the input variable.
+const vowels = ['A', 'E', 'I', 'O', 'U'];
+
+// This will serve as a place to store the vowels from the input string.
+let resultArray = [];
+
+// This outer loop iterates through each letter of the input variable text.
+for (let i = 0; i < input.length; i++) {
+  // This inner loop iterates through each vowel.
+  for (let j = 0; j < vowels.length; j++) {
+    // This if statement compares the input letter to every letter in the vowels array.
+    if (input[i] === vowels[j]) {
+      resultArray.push(input[i]);
+    }
+  }
+  // This checks if each letter in the input string is equal to 'e' or 'u'. If so, it's doubled.
+  if (input[i] === 'E' || input[i] === 'U') {
+    resultArray.push(input[i]);
+  }
+}
+// Here we log the array. As the output has commas between each letter, we can use the method .join('') to take out these commas.
+console.log(resultArray.join(''));
+
